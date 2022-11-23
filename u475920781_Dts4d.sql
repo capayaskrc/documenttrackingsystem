186dfef1b633594bc42861351e92f126e91f8b8d
Jjhay83 <118253806+Jjhay83@users.noreply.github.com>
2022-11-23 21:22:13 +0800

    Update and rename dtsystem.sql to u475920781_Dts4d.sql

--- u475920781_Dts4d.sql
+++ u475920781_Dts4d.sql
@@ -1,6 +1,6 @@
 /*
 SQLyog Community v13.1.9 (64 bit)
-MySQL - 10.4.24-MariaDB : Database - dtsystem
+MySQL - 10.4.24-MariaDB : Database - u475920781_Dts4d
 *********************************************************************
 */
 
@@ -12,9 +12,9 @@ MySQL - 10.4.24-MariaDB : Database - dtsystem
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`dtsystem` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`u475920781_Dts4d` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 
-USE `dtsystem`;
+USE `u475920781_Dts4d`;
 
 /*Table structure for table `document_fields` */
 
