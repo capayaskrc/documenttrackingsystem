432b05225726fc9c19562a600c54a5e7ec6237d0
charlieJenson <charlesjenson.caingal@student.dmmmsu.edu.ph>
2022-12-13 21:21:41 +0800

    Updated User page functions

--- script/user.js
+++ script/user.js
@@ -10,7 +10,7 @@ function displayData() {
                 row = row +
                     "<tr id='" + json.data[i].userid + "'>" + "<td><span class='custom-checkbox'><input type='checkbox' id='selectAll'><label for='selectAll'></label></span></td>" +
                     "<td>" + json.data[i].username + "</td><td>" + json.data[i].name + "</td>" +
-                    "<td>" + json.data[i].role + "</td><td>" + json.data[i].position + "</td>" +
+                    "<td>" + json.data[i].school + "</td><td>" + json.data[i].position + "</td>" +
                     "<td>" + json.data[i].status + "</td>" +
                     "<td><a href='#editDocumentModal' class='edit' data-toggle='modal'><i class='material-icons' data-toggle='tooltip' title='Edit'>&#xE254;</i>" +
                     "</a><a href='#deleteDocumentModal' class='delete' data-toggle='modal'><i class='material-icons' data-toggle='tooltip' title='Delete'>&#xE872;</i></a></td>" +
@@ -20,11 +20,28 @@ function displayData() {
             $("#data").get(0).innerHTML = row;
         });
 }
+
+function getSchool() {
+    $.post("http://localhost/dts_api/dtsapi/DocTS/api/public/fetchSchools",
+        function (data, status) {
+            var json = JSON.parse(data);
+            var row = "<option disabled selected>Select School</option>";
+            for (var i = 0; i < json.data.length; i++) {
+
+                row = row + '<option value="' + json.data[i].school_name + '">' + json.data[i].school_name + '</option>';
+
+            }
+            $("#school").get(0).innerHTML = row;
+        });
+}
+
 //fetch data from database using API
 $(document).ready(function () {
     $(window).on("load", function () {
         displayData();
     });
+
+    getSchool();
 
     // Save data to the database using api
     $(document).ready(function () {
@@ -32,10 +49,11 @@ $(document).ready(function () {
             var id = (new Date().getTime()).toString(10)
             var username = $("#username").get(0).value;
             var name = $("#name").get(0).value;
-            var role = $("#pillSelect").get(0).value;
+            var role = $("#role").get(0).value;
             var position = $("#position").get(0).value;
             var status = $("#status").get(0).value.toString();
             var email = $("#email").get(0).value;
+            var school = $("#school").get(0).value;
             // var tag = $("#tag").get(0).value;
             $.post("http://localhost/dts_api/dtsapi/DocTS/api/public/addUser",
                 JSON.stringify({
@@ -43,10 +61,11 @@ $(document).ready(function () {
                     username: username,
                     name: name,
                     email: email,
-                    pp: "profile_pic",
+                    pp: "http://localhost/dts_api/dtsapi/DocTS/api/images/default.png",
                     role: role,
                     position: position,
                     status: status,
+                    school: school,
                     password: "1234567890"
                 }),
                 function (data, status) {
@@ -75,7 +94,7 @@ $(document).ready(function () {
                     var json = JSON.parse(data);
                     $("#data").get(0).innerHTML = "<tr id='" + json.data[0].userid + "'>" + "<td><span class='custom-checkbox'><input type='checkbox' id='selectAll'><label for='selectAll'></label></span></td>" +
                         "<td>" + json.data[0].username + "</td><td>" + json.data[0].name + "</td>" +
-                        "<td>" + json.data[0].role + "</td><td>" + json.data[0].position + "</td>" +
+                        "<td>" + json.data[0].school + "</td><td>" + json.data[0].position + "</td>" +
                         "<td>" + json.data[0].status + "</td>" +
                         "<td><a href='#editDocumentModal' class='edit' data-toggle='modal'><i class='material-icons' data-toggle='tooltip' title='Edit'>&#xE254;</i>" +
                         "</a><a href='#deleteDocumentModal' class='delete' data-toggle='modal'><i class='material-icons' data-toggle='tooltip' title='Delete'>&#xE872;</i></a></td>" +
